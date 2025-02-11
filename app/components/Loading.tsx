9dcb021768fe7e3d46e7b9c51fe25f10cabc22db
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-02-11 15:37:58 +0300

    Add loading component with skeleton placeholders and update package dependencies

---
+++ app/components/Loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Table } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const Loading = () => {
+  const issues = [1, 2, 3, 4, 5];
+  return (
+    <div>loading ...</div>
+  );
+};
+
+export default Loading;
