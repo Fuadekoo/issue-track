a47c8dcab116633e27e7559f5a024054a9770271
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-02-15 11:34:43 +0300

    Add Tailwind typography plugin and integrate ReactMarkdown for issue descriptions

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,7 +4,7 @@ import { notFound } from "next/navigation";
 import delay from "delay";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-
+import ReactMarkdown from "react-markdown";
 const prisma = new PrismaClient();
 
 interface Props {
@@ -28,8 +28,8 @@ const IssueDetailPage = async ({ params }: Props) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.description}</p>
+      <Card className="prose mt-4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   );
