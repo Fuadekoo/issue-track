fcc915038ce00de1eb7a026720fb6fd43b746b09
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-02-15 12:17:11 +0300

    Refactor Link component to use custom Link wrapper for improved consistency

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import Link from "../components/Link";
+// import Link from "next/link";
 import { prisma } from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
