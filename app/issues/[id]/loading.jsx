c928ecd5a82b259185f2953c2790a303ac9c49ea
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-02-15 12:26:19 +0300

    Enhance loading component with skeleton placeholders for improved user experience

--- app/issues/[id]/loading.jsx
+++ app/issues/[id]/loading.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
+import { Heading, Flex, Card, Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 const loading = () => {
-  return <div>loading...</div>;
+  return (
+    <Box>
+      <Skeleton />
+      <Flex className="space-x-3" my="2">
+        <Skeleton width="5 rem" />
+        <Skeleton />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count="3" />
+      </Card>
+    </Box>
+  );
 };
 
 export default loading;
